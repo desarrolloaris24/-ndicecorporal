6e91d27ef4887dec065c62a5b78a70d620c8a592
Daniela Aristizabal <desarrollodesoftwaredaniela@gmail.com>
2025-02-26 14:55:12 -0500

    cambio formulario

---
+++ controldor.js
@@ -0,0 +1,7 @@
+//alert("estoy vinculado")
+
+//crear variables 
+
+let inputPesoUsuario=document.getElementById("pesousuario")
+let inputAltursUsuario=document.getElementById("alturausuario")
+let botonFormulario=document.getElementById("boton")
